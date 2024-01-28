75cecbf70945f3525fde386d8e1a76aa45a0bbbc
Charlie Page <charles.david.page@gmail.com>
2024-01-28 08:58:46 -0600

    clean up page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import './styles.css';
-    import screenshots from '$lib/images/app-screenshots.png';
+    import screenshots from '$lib/images/landing-page-screenshots-narrow.svg';
+    import googlebadge from '$lib/images/google-play-badge.svg';
 </script>
 
 <h1>Nothing Great Was Ever Achieved Alone</h1>
@@ -8,10 +9,10 @@
     <div class="hero-cta">
         <p>Use the power of accountability to follow your long-term goals in the company of friends.</p>
         <div class="app-store-links">
-                    <a class="store-link apple-link" href="https://apps.apple.com/us/app/postdrafts/id6455785683?itsct=apps_box_badge&amp;itscg=30200">
+                    <a class="store-link apple-link" href="https://apps.apple.com/us/app/lattis-goal-habit-tracker/id6470149396">
                         <img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1604880000" alt="Download on the App Store">
                     </a>
-            <a class="store-link" href='https://play.google.com/store/apps/details?id=com.pagefamdesigns.lattis&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+            <a class="store-link-google" href='https://play.google.com/store/apps/details?id=com.pagefamdesigns.lattis&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src={googlebadge}/></a>
         </div>
     </div>
     <img src={screenshots} alt="SvelteKit screenshots" />
@@ -19,24 +20,56 @@
 
 <style>
     .hero {
-        display: flex;
+        display: inline-block;
         align-items: top;
     }
 
     .hero-cta {
-        width: 50%;
+        width: 100%;
     }
 
     .hero p {
         font-size: 1.5em;
-        text-align: left;
+        text-align: center;
     }
 
     .hero img {
-        width: 50%;
+        width: 100%;
         max-width: 600px;
         border-radius: 10px;
         margin: 2em 0;
+    }
+
+    @media (min-width: 750px) {
+        
+
+        .hero {
+            display: flex;
+        }
+
+        .hero img {
+            width: 50%;
+        }
+
+    }
+
+    @media (min-width: 975px) {
+        
+
+        .hero {
+            align-items: center;
+        }
+
+        .hero-cta {
+            align-items: center;
+            width: 50%;
+        }
+
+        .hero p {
+            text-align: left;
+        }
+
+
     }
 
     .app-store-links {
@@ -47,6 +80,7 @@
 
     .store-link {
         display: flex;
+        padding: 0 1em;
 
     }
 
@@ -54,6 +88,13 @@
         width: 200px;
         height: 66px;
         object-fit: contain;
+    }
+
+    .store-link-google img {
+        width: 220px;
+        height: 66px;
+        object-fit: contain;
+        padding-top: 4px;
     }
 
 </style>
