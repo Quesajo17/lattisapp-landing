3e40d2c9b5acaefb9ca62ee89226abb8685e8d45
Charlie Page <charles.david.page@gmail.com>
2024-01-28 20:55:49 -0600

    update privacy policy and support

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -6,13 +6,17 @@
 <div class="text-column">
 	<h1>Privacy Policy</h1>
 
-    <h3>Overview</h3>
-	<p>
-		We collect data.
-    </p>
-    <p>
-        <a href="mailto:charlie@lattisapp.com">charlie@lattisapp.com</a>
-	</p>
-
+    <h2>Overview</h2>
+	<p>Your data is stored on the cloud, via Google Firebase. Administrators of the Lattis app can see your data and will access it only to troubleshoot issues with the app.</p>
+	<h2>Who can see my data?</h2>
+	<p><b>You</b>. You have full access to your data within the app, and can modify it and see it as needed. <b>App administrators.</b> App administrators can see your data and will access it only to troubleshoot issues with the app. Deidentified data (such as the number of habits per user) may also be reported on by app administrators. <b>Others in my groups.</b> People you join groups with have the ability to see your task/goal/habits and completions. This data will not be available to anyone who is not actively part of a group that you are an active member of. Future privacy enhancements may give you more fine-grained control over which items of yours are available to groups and which are not.</p>
+	<h2>What data is collected?</h2>
+	<p>When you sign up for an account, we collect your email used to sign up, as well as name and profile picture (if you provide it). As you use the app, data you input regarding long-term goals, habits, and tasks is stored in our server on Google Firebase and is tied to your user record.</p>
+	<h2>Where is my data collected?</h2>
+	<p>We have two places your data is currently stored: Almost all of your data (everything mentioned in the previous paragraph) is stored in Google Firebase (see their <a href="https://firebase.google.com/support/privacy">privacy policy</a> for more info). We store information related to your user record and any current subscription information in a service called RevenueCat. See their <a href="https://www.revenuecat.com/privacy/">privacy policy</a> for more information on their data storage.</p>
+	<h2>How is my data used?</h2>
+	<p>Your data is used to provide you with the services of the app. We do not sell your data to third parties. We may use deidentified data (such as the number of habits per user) to report on app usage. Otherwise your identified data is only used for troubleshooting purposes to help make sure your experience with the app is positive.</p>
+	<h2>How do I delete my data?</h2>
+	<p>You can delete your account from within the app. Make sure you are logged in, navigate to the profile page by pressing the person icon in the upper right-hand corner of the app. There is a "Need Help?" button at the bottom, and from here you will see instructions to delete your account.</p>
 	
 </div>
--- src/routes/support/+page.svelte
+++ src/routes/support/+page.svelte
@@ -6,12 +6,17 @@
 <div class="text-column">
 	<h1>Support</h1>
 
-	<p>
-		Please direct all feedback and support questions to:
-    </p>
+	<p>See FAQ below for common questions. If your question is not answered below, please direct all questions and support requests to:</p>
+
     <p>
         <a href="mailto:charlie@lattisapp.com">charlie@lattisapp.com</a>
 	</p>
 
+	<h2>FAQ</h2>
+
+	<h3>How do I delete my account?</h3>
+
+	<p>You can delete your account from within the app. Make sure you are logged in, navigate to the profile page by pressing the person icon in the upper right-hand corner of the app. There is a "Need Help?" button at the bottom, and from here you will see instructions to delete your account.</p>
+	<p>If you have any issues, please email <a href="mailto:charlie@lattisapp.com">charlie@lattisapp.com</a>.</p>
 	
 </div>
