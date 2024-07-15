22e796ebca4b2581005365d3c116b37995c0e33d
Charlie Page <charles.david.page@gmail.com>
2024-07-15 10:56:52 -0500

    added resources page

---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Resources</title>
+	<meta name="description" content="Resources" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Resources</h1>
+    <p>Coming soon: writings and other resources on setting up accountability groups.</p>
+</div>
